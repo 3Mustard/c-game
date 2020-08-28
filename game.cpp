6895e1b990881e127c642f16fec4005e1671452d
Matt <m23.cullen@gmail.com>
2020-08-28 13:12:13 -0400

    example of a new line

--- game.cpp
+++ game.cpp
@@ -2,6 +2,10 @@
 
 int main() 
 {
-  std::cout << "Hello, World";
+  std::cout << "You wake up in a small room unsure of how you got there...";
+  std::cout << std::endl;
+  std::cout << "The only feature is a door with a small terminal next to it. ";
+  std::cout << std::endl;
+  std::cout << "The terminal reads: Please enter the correct code to escape.";
   return 0;
 }
