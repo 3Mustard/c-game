f0acd10b9e9ecbe3c912d61c6614132af00ee53a
Matt <m23.cullen@gmail.com>
2020-08-28 15:36:04 -0400

    refactored all the system in and out prints

--- game.cpp
+++ game.cpp
@@ -3,35 +3,43 @@
 int main() 
 {
 
-  const int CodeA = 2;
-  const int CodeB = 3;
-  const int CodeC = 6;
+  // Declare and assign numbers to variables
+  const int CodeX = 2;
+  const int CodeY = 3;
+  const int CodeZ = 6;
 
-  const int CodeSum = CodeA + CodeB + CodeC;
-  const int CodeProduct = CodeA * CodeB * CodeC;
+  const int CodeSum = CodeX + CodeY + CodeZ;
+  const int CodeProduct = CodeX * CodeY * CodeZ;
 
+  // Print the code product and sum to the terminal 
+  std::cout << "You wake up in a small room unsure of how you got there...\n";
+  std::cout << "The only feature is a door with a small terminal next to it. \n";
+  std::cout << "The terminal reads: \"Enter the Code * * *\"";
+  std::cout << "\n~ The sum of * * * is " << CodeSum;
+  std::cout << "\n~ The product of * * * is " << CodeProduct << std::endl;
 
-  std::cout << "You wake up in a small room unsure of how you got there..." << std::endl;
-  std::cout << "The only feature is a door with a small terminal next to it. " << std::endl;
-  std::cout << std::endl;
-  std::cout << "The terminal reads: " << std::endl;
-  std::cout << "~ There are 3 numbers in the code" << std::endl;
-  std::cout << "~ The numbers add-up to: " << CodeSum << std::endl;
-  std::cout << "~ The codes multiply to give: " << CodeProduct << std::endl;
+  // Store user guesses
+  int GuessX, GuessY, GuessZ;
+  std::cout << "~ *: ";
+  std::cin >> GuessX;
+  std::cout << "~ *: ";
+  std::cin >> GuessY;
+  std::cout << "~ *: ";
+  std::cin >> GuessZ; 
 
-  int GuessA, GuessB, GuessC;
-  std::cout << ">> ";
-  std::cin >> GuessA;
-  std::cin >> GuessB;
-  std::cin >> GuessC; 
+  int GuessSum = GuessX + GuessY + GuessZ;
+  int GuessProduct = GuessX * GuessY * GuessZ;
 
-  int GuessSum = GuessA + GuessB + GuessC;
-  int GuessProduct = GuessA * GuessB * GuessC;
-
-  if (GuessSum == CodeSum)
+  // Determine if the player won or lost
+  if (GuessSum == CodeSum && GuessProduct == CodeProduct)
   {
     std::cout << "You win!";
   }
+  else 
+  {
+    std::cout << "You lose!";
+  }
 
+  // return statement here 
   return 0;
 }
